faa4ed81e8f3cf7f6a1a56d899e5edfec7823a8d
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-20 18:22:57 -0400

    navbar create

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[11vw] 2xl:px-[15vw]">
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/"element={<Home></Home>} />
         <Route path="/collection" element={<Collection />} />
