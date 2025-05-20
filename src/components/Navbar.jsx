11a6ba139de1a91c58b3a6945fecd041d7d8f47d
ShamimHossain1 <shamimweb78@gmail.com>
2025-05-20 18:53:27 -0400

    nav bar for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
 import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex items-center justify-between py-5 font-medium">
+      {/* Navbar Logo */}
       <img src={assets.logo} alt="Logo" className="w-36" />
+
+      {/* Navbar Links */}
+
       <ul className="hidden sm:flex gap-5  text-gray-700 ">
         <NavLink to="/" className="flex flex-col items-center gap-1">
           <p>Home</p>
@@ -23,6 +28,8 @@ const Navbar = () => {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
+
+      {/* Navbar Search and Profile */}
       <div className="flex items-center gap-6">
         <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
         <div className="group relative ">
@@ -41,8 +48,47 @@ const Navbar = () => {
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center text-white bg-black rounded-full leading-4 aspect-square text-[9px]">110</p>
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center text-white bg-black rounded-full leading-4 aspect-square text-[9px]">
+            110
+          </p>
         </Link>
+
+        {/* Mobile Menu */}
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 sm:hidden cursor-pointer"
+          alt=""
+        />
+
+        {/* Side bar menu for small screens */}
+
+        <div
+          className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-gray-50 transition-all ${
+            visible ? "w-3/5" : "w-0"
+          } `}
+        >
+          <div className="flex flex-col text-gray-600">
+            {/* Go Back */}
+            <div
+              onClick={() => setVisible(false)}
+              className="flex items-center gap-4 p-3 cursor-pointer"
+            >
+              <img
+                src={assets.dropdown_icon}
+                className="h-4 rotate-180"
+                alt=""
+              />
+              <p>Back</p>
+            </div>
+            {/* Links */}
+            <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b-[1px] border-gray-300 hover:bg-gray-100 " to="/">Home</NavLink>
+            <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b-[1px] border-gray-300 hover:bg-gray-100 " to="/collection">Collection</NavLink>
+            <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b-[1px] border-gray-300 hover:bg-gray-100 " to="/about">About</NavLink>
+            <NavLink onClick={() => setVisible(false)} className="py-2 pl-6 border-b-[1px] border-gray-300 hover:bg-gray-100 " to="/contact">Contact</NavLink >
+
+          </div>
+        </div>
       </div>
     </div>
   );
